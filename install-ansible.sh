6ada27c8cdf8305e2290301886ac7686b29bfc34
Radoslav <125762359+stoimenovrado@users.noreply.github.com>
2023-09-15 11:34:18 +0300

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,9 +1,9 @@
 #bin $bash
 
 echo "* Install ansible ..."
-apt update
-apt install software-properties-common -y
-add-apt-repository --yes --update ppa:ansible/ansible
-apt install ansible -y
-apt install sshpass -y
-apt install vim -y
+sudo apt update
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+sudo apt install sshpass -y
+sudo apt install vim -y
